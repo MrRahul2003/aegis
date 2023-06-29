6d25485497eb0182553754bdd942ee4bd720aeb8
MrRahul <99179217+MrRahul2003@users.noreply.github.com>
2023-06-29 21:07:59 +0530

    push

--- routes/Route.js
+++ routes/Route.js
@@ -3,20 +3,15 @@ import express from "express";
 // controllers
 import { addContact } from "../controller/ContactController.js";
 import { sendMail } from "../controller/MailController.js";
-// import { addBlog, showBlogs } from "../controller/BlogController.js";
 import { addResume } from "../controller/CareerController.js";
 
 // middleware
-// import { storage, upload } from "../middleware/BlogMiddleware.js";
 import { storage, upload } from "../middleware/CareerMiddleware.js";
 
 const route = express.Router();
 
 route.post("/contact/add", addContact);
 route.post("/contact/sendmail", sendMail);
-
-// route.post("/blog/addblog", upload.single(["photo"]), addBlog);
-// route.get("/blog/showblogs", showBlogs);
 
 route.post("/resume/addresume", upload.single(["resume"]), addResume);
 
